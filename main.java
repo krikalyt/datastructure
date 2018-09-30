d3feec0250d5ead37e8978d08c7949db20bd1b96
Krishna Kumar Yadav <krikalyt@gmail.com>
2018-10-01 01:09:07 +0530

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ package datastructurebyKrishna;
 public class main {
     public static void main(String[] args) throws Exception{
         PriorityQueue priorityQueue = new PriorityQueue();
-        priorityQueue.getHighestPriority();
+        //do yours
     }
 }
