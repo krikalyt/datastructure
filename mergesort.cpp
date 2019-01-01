0d74fa8aea809b0b20166eeaf6ec6a77ec6af820
Krishna Kumar Yadav <krikalyt@gmail.com>
2019-01-01 23:31:11 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,53 +1,76 @@
-void merge(int arr[], int l, int m, int r) 
-{ 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
-  
-    /* create temp arrays */
-    int L[n1], R[n2]; 
-  
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
-  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray 
-    j = 0; // Initial index of second subarray 
-    k = l; // Initial index of merged subarray 
-    while (i < n1 && j < n2) 
-    { 
-        if (L[i] <= R[j]) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of L[], if there 
-       are any */
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of R[], if there 
-       are any */
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
-} 
-  
+#include <iostream>
+using namespace std;
+void merge(int arr[], int start, int middle, int last);
+void mergesort(int arr[], int start, int last);
+int main(int argm,char **args)
+{
+	int arr[] = { 8,22,10};
+	merge(arr,0,1,2);
+	for(int i=0;i<=2;i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+	return 0;
+}
+void mergesort(int arr[], int start, int last)
+{
+	if (start < last)
+	{
+		int middle = start + ((last - start) / 2);
+		mergesort(arr, start, middle);
+		mergesort(arr, middle+1, last);
+		merge(arr, start, middle, last);
+	}
+}
+void merge(int arr[], int start, int middle, int last)
+{
+	int sizeofone = middle - start + 1;
+	int sizeoftwo = last - middle;
+	int arr1[sizeofone];
+	int arr2[sizeoftwo];
+
+	for (int i = 0; i < sizeofone; i++)
+	{
+		arr1[i] = arr[i];
+	}
+
+	for (int i = 0; i < sizeoftwo; i++)
+	{
+		arr2[i] = arr[middle+1+i];
+	}
+
+	int totalsize = sizeofone + sizeoftwo;
+	int f = 0;
+	int s = 0;
+    int i= start;
+
+    while(f <= middle && s <= last)
+    {
+        if(arr1[f]<=arr2[s])
+        {
+            arr[i] = arr1[f];
+            f++;
+        }
+        else{
+            arr[i] = arr2[s];
+            s++;
+        }
+        i++;
+    }
+    i--;
+
+    while(f <= middle)
+    {
+        arr[i] = arr1[f];
+        f++;
+        i++;
+    }
+
+    while(s <= last)
+    {
+        arr[i] = arr1[s];
+        s++;
+        i++;
+    }
+}
